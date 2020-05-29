179bca312447d2f6619bb7209c7c709eb2ea87e3
Mamadou Billo Diallo <billodiallonet@gmail.com>
2020-05-29 13:19:48 +0300

    form for newprofile

---
+++ images/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm *
+
+class NewProfile(forms.ModelForm):
+    class Meta:
+        model = Profile 
+        exclude = ['user']
+
+class        
