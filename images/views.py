d5a93d8b8adeb686fffcbbc4c590d47386de5eb1
Mamadou Billo Diallo <billodiallonet@gmail.com>
2020-05-29 14:44:03 +0300

    adding sign up function

--- images/views.py
+++ images/views.py
@@ -13,3 +13,27 @@ from django.contrib.auth.models import User
 from django.core.mail import EmailMessage
 
 # Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            current_user = form.save(commit=False)
+            current_user.is_active = False
+            current_user.save()
+            current_site = get_current_site(request)
+            mail_subject = 'Activate your instapicha account.'
+            message = render_to_string('acc_active_email.html', {
+                'user': current_user,
+                'domain': current_site.domain,
+                'uid':urlsafe_base64_encode(force_bytes(current_user.pk)),
+                'token':account_activation_token.make_token(current_user),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(
+                        mail_subject, message, to=[to_email]
+            )
+            email.send()
+            return HttpResponse('Please confirm your email address to complete the registration')
+    else:
+        form = SignupForm()
+    return render(request, 'signup.html', {'form': form})
