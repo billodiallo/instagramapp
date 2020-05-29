daabb832534febc013ad22524989fb8dc5480fa1
Mamadou Billo Diallo <billodiallonet@gmail.com>
2020-05-29 11:35:58 +0300

    inport User from from django.contrib.auth.models

--- images/models.py
+++ images/models.py
@@ -1,5 +1,5 @@
-#from django.db import models
-f#rom django.contrib.auth.models import User
+from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
