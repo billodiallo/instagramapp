4360d7d4b609e5149d355391e24875c081d44d9b
Mamadou Billo Diallo <billodiallonet@gmail.com>
2020-05-29 11:42:20 +0300

    save_profile fix spelling error

--- images/models.py
+++ images/models.py
@@ -5,10 +5,10 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,null=True)
-    profile_photo=models.ImageField(upload_to='profiles/ ,null=True)
+    profile_photo=models.ImageField(upload_to='profiles/',null=True)
     bio= models.CharField(max_length=240,null=True)
 
-    def save_profile():
+    def save_profile(self):
         self.save()
     @classmethod
     def get_profile(cls):
