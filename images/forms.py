93d19a2dd3eb669222ba809f0747d479b7816a0f
Mamadou Billo Diallo <billodiallonet@gmail.com>
2020-05-29 13:30:01 +0300

    signupform

--- images/forms.py
+++ images/forms.py
@@ -15,4 +15,10 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         exclude =['poster','image']
-        
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=200, help_text='Required')
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+       
