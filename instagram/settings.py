ed106c082d2819f6e004a1ba60066706f653756c
Mamadou Billo Diallo <billodiallonet@gmail.com>
2020-06-01 19:13:56 +0300

    ss

--- instagram/settings.py
+++ instagram/settings.py
@@ -174,12 +174,13 @@ LOGIN_REDIRECT_URL='home'
 
 # Email configurations remember to install python-decouple
 
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_PORT = 25
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'billodiallonet@gmail.com'
+EMAIL_HOST_PASSWORD = 'passe23456'
+ 
 
 
 # Configure Django App for Heroku.
