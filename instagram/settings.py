5a762cf45c572d9770e548a8770ae15d56b02b64
Mamadou Billo Diallo <billodiallonet@gmail.com>
2020-06-01 19:14:48 +0300

    ss

--- instagram/settings.py
+++ instagram/settings.py
@@ -179,7 +179,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'billodiallonet@gmail.com'
-EMAIL_HOST_PASSWORD = 'passe23456'
+EMAIL_HOST_PASSWORD = 'assalidantec'
  
 
 
