a422bdf4132c68543517b264d972782bcd82d202
Mamadou Billo Diallo <billodiallonet@gmail.com>
2020-06-02 18:17:07 +0300

    update settings

--- instagram/settings.py
+++ instagram/settings.py
@@ -174,14 +174,12 @@ LOGIN_REDIRECT_URL='home'
 
 # Email configurations remember to install python-decouple
 
-
 EMAIL_USE_TLS = config('EMAIL_USE_TLS')
 EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
- 
 
 
 # Configure Django App for Heroku.
